5de21a337a3c0a48c351f73a0ec9b00bfa91493b
Andres Pedraza <andrespedca@gmail.com>
2026-07-04 22:14:25 -0500

    Implemented tokenize text, and now the tool is able to search for partial matches

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,5 +1,17 @@
 import argparse
 import json
+import os
+from pathlib import Path
+from typing import TypedDict
+import string
+
+class Movie(TypedDict):
+    id: int
+    title: str
+    description: str
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -13,20 +25,46 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            with open("data/movies.json") as file:
-                data = json.load(file)
-                i: int = 0
-                limit: int = 5
-                for v in data["movies"]:
-                    if args.query in v["title"]: 
-                        print(f"{i + 1}. {v["title"]}")
-                        i += 1
-                        if i == limit:
-                            break
+            search_command(args.query)
 
         case _:
             parser.print_help()
 
+
+def search_command(query: str, limit: int = 5):
+    movies = load_movies()
+    i: int = 0
+    query_processed: list[str] = preprocess_text(query)
+    found: bool = False
+
+    for movie in movies:
+        if i == limit:
+            break
+        for word in query_processed:
+            for wtitle in preprocess_text(movie["title"]):
+                if word in wtitle:
+                    print(f"{i + 1}. {movie["title"]}")
+                    i += 1
+                    found = True
+                    break
+            if found:
+                found = False
+                break
+            
+    
+
+def load_movies() ->list[Movie]:
+    with open(DATA_PATH) as file:
+        data = json.load(file)
+    return data["movies"]
+
+def preprocess_text(input: str) -> list[str]:
+    result = input.lower()
+    to_remove: str = string.punctuation
+    table = str.maketrans("", "", to_remove)
+    result = result.translate(table)
+    result = list(filter(None, result.split()))
+    return result
 
 if __name__ == "__main__":
     main()
