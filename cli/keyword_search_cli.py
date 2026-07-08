dbe81fcb72ee8f1046164d0f4bf301cc4442775c
Andres Pedraza <andrespedca@gmail.com>
2026-07-07 19:45:36 -0500

    Fixed bug where it was saving the same dict for the two cache files now loading from disk and using that cache to search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -7,6 +7,12 @@ import string
 from nltk.stem import PorterStemmer
 from collections import defaultdict
 import pickle
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+CACHE_PATH = os.path.join(PROJECT_ROOT, "cache")
+STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
 class Movie(TypedDict):
     id: int
@@ -27,8 +33,7 @@ class InvertedIndex:
 
     def get_documents(self, term: str) -> list[int]:
         result: list[int] = list(self.index[term])
-        result.sort()
-        return result
+        return sorted(result)
 
     def build(self) -> None:
         movies: list[Movie] = load_movies()
@@ -45,62 +50,47 @@ class InvertedIndex:
             pickle.dump(self.index, f)
 
         with open(docmap_path, "wb") as f:
-            pickle.dump(self.index, f)
+            pickle.dump(self.docmap, f)
+
+    def load(self) -> None:
+        index_path: str = os.path.join(CACHE_PATH, "index.pkl")
+        docmap_path: str = os.path.join(CACHE_PATH, "docmap.pkl")
+
+        try:
+            with open(index_path, "rb") as f1:
+                self.index = pickle.load(f1)
+        except FileNotFoundError:
+            print(f"Error: file {index_path} does not exist")
+            sys.exit()
+
+        try:
+            with open(docmap_path, "rb") as f2:
+                self.docmap = pickle.load(f2)
+        except FileNotFoundError:
+            print(f"Error: file {docmap_path} does not exist")
+            sys.exit()
             
-        
-    
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
-CACHE_PATH = os.path.join(PROJECT_ROOT, "cache")
-STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
-
-
-
-def main() -> None:
-    parser = argparse.ArgumentParser(description="Keyword Search CLI")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
-
-    search_parser = subparsers.add_parser("search", help="Search movies using keywords")
-    subparsers.add_parser("build", help="Build the index reverse")
-    search_parser.add_argument("query", type=str, help="Search query")
-
-    args = parser.parse_args()
-
-    match args.command:
-        case "search":
-            print(f"Searching for: {args.query}")
-            search_command(args.query)
-        case "build":
-            i = InvertedIndex()
-            i.build()
-            i.save()
-            print(i.get_documents("merida")[0])
-
-        case _:
-            parser.print_help()
-
-
 def search_command(query: str, limit: int = 5):
-    movies = load_movies()
+    iidx = InvertedIndex()
+    iidx.load()
     i: int = 0
     query_processed: list[str] = tokenize_text(query)
-    found: bool = False
+    results: list[int] = []
 
-    for movie in movies:
+    for qsearch in query_processed:
+        response: list[int] = iidx.get_documents(qsearch)
+        if not response:
+            continue
+        else:
+            results.extend(response)
+
+
+    for key in results:
+        movie = iidx.docmap[key]
+        print(f"{i + 1}. ({movie["id"]}) {movie["title"]}")
+        i += 1
         if i == limit:
             break
-        for word in query_processed:
-            for wtitle in tokenize_text(movie["title"]):
-                if word in wtitle:
-                    print(f"{i + 1}. ({movie['id']}) {movie["title"]}")
-                    i += 1
-                    found = True
-                    break
-            if found:
-                found = False
-                break
-            
-    
 
 def load_movies() ->list[Movie]:
     with open(DATA_PATH) as file:
@@ -135,5 +125,28 @@ def tokenize_text(input: str) -> list[str]:
     result = list(filter(lambda x: x not in STOPWORDS, result))
     result = [stemmer.stem(tok) for tok in result]
     return result
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using keywords")
+    subparsers.add_parser("build", help="Build the index reverse")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            print(f"Searching for: {args.query}")
+            search_command(args.query)
+        case "build":
+            i = InvertedIndex()
+            i.build()
+            i.save()
+
+        case _:
+            parser.print_help()
+
 if __name__ == "__main__":
     main()
