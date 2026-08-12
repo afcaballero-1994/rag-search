3ed7403ddd6f30fc9d3f143fa56334b2485bceb6
Andres Pedraza <andrespedca@gmail.com>
2026-08-11 23:44:03 -0500

    Generate embeddings with document information, to refactor later

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,21 +1,59 @@
+import os
 from sentence_transformers import SentenceTransformer
+import numpy as np
+import numpy.typing as npt
+from numpy.typing import NDArray
+from typing import Any
+from pathlib import Path
+
+EmbeddingArray = NDArray[Any]
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+CACHE_PATH = os.path.join(PROJECT_ROOT, "cache")
 
 class SemanticSearch:
+    file_path = os.path.join(CACHE_PATH, "movie_embeddings.npy")
 
     def __init__(self, model_name: str = "all-MiniLM-L6-v2") -> None:
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
 
     def generate_embedding(self, text: str):
         if not text:
             raise ValueError("provide no empty String")
         
-        embeddings = self.model.encode([text])
+        return self.model.encode([text])[0]
 
-        print(type(embeddings))
-        print(type(embeddings[0]))
+    def build_embeddings(self, documents: list[dict]) -> npt.ArrayLike:
+        self.documents = documents
+        rmovie: list[str] = []
 
-        return embeddings[0]
-        
+        for document in documents:
+            self.document_map[document['id']] = document
+            rmovie.append(f"{document['title']}: {document['description']}")
+
+        self.embeddings = self.model.encode(rmovie, show_progress_bar=True)
+
+        with open(self.file_path, 'wb') as f:
+            np.save(f, self.embeddings)
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents: list[dict]):
+        self.documents = documents
+
+        for doc in documents:
+            for k, _ in doc.items():
+                self.document_map[k] = doc
+
+        if os.path.exists(self.file_path):
+            self.embeddings = np.load(self.file_path)
+            if len(self.embeddings) == len(documents):
+                return self.embeddings
+        else:
+            return self.build_embeddings(documents)
 
 def verify_model() -> None:
     m = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,6 +1,17 @@
 import argparse
-
+import json
+import os
+from pathlib import Path
+from keyword_search_cli import Movie
 from lib import semantic_search
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+
+def load_movies() ->list[Movie]:
+    with open(DATA_PATH) as file:
+        data = json.load(file)
+    return data["movies"]
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic search CLI")
@@ -10,6 +21,8 @@ def main() -> None:
 
     embed_text_parser = subparsers.add_parser("embed_text", help="Generate embedding")
     embed_text_parser.add_argument("text", type=str, help="Text used to generate emb")
+
+    verify_embeddings_parser = subparsers.add_parser("verify_embeddings", help="Verify")
     
     args = parser.parse_args()
 
@@ -20,9 +33,20 @@ def main() -> None:
             semantic_search.verify_model()
         case "embed_text":
             semantic_search.embed_text(args.text)
+        case "verify_embeddings":
+            movies = load_movies()
+            m = semantic_search.SemanticSearch()
+            m.load_or_create_embeddings(movies)
+
+            print(f"Number of docs: {len(m.documents)}")
+            print(
+                f"Embeddings shape: {m.embeddings.shape[0]} vectors in {m.embeddings.shape[1]} dimensions"
+            )
+            
         case _:
             parser.print_help()
 
 
 if __name__ == "__main__":
     main()
+
